ce804fa20ffd0385db954ac51f034a614d1af9ca
Wako <dulo.rodrigo@gmail.com>
2020-02-17 16:52:40 -0600

    Comienza página cv

--- src/app/paginas/cv/cv.component.ts
+++ src/app/paginas/cv/cv.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
+  skills: string[] = ["Angular", "Apache", "Bash", "Docker", "Express", "Git", "Javascript", "Linux", "MongoDB", "MySQL", "Node.js", "PHP", "PostgreSQL", "Socket.IO", "TypeScript", "Wordpress"]
+
   constructor() { }
 
   ngOnInit(): void {
